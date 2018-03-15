1b02eba7adca44358899530efa3fae51718168f5
leannevandam <36501611+leannevandam@users.noreply.github.com>
2018-03-15 12:11:45 +0100

    Delete sorting eigenvalues.py

--- sorting eigenvalues.py
+++
@@ -1,4 +0,0 @@
-def sortedeigenvalues(M): #insert a 'matrix' like a array as [[a,b,c],[d,e,f],[g,h,i]], so the rows of the matrix as seperate lists
-    evalues,y = linalg.eig(M)
-    evalues = np.sort(evalues)
-    return evalues
