91122e79ec2e366cf8cbd6a0ab8c15a110632ef9
leannevandam <36501611+leannevandam@users.noreply.github.com>
2018-03-15 12:11:37 +0100

    Delete computing S and Omega.py

--- computing S and Omega.py
+++
@@ -1,9 +0,0 @@
-def computeSandO(J): # J has to be a 3x3 array
-    S = np.array([[0.,0.,0.],[0.,0.,0.],[0.,0.,0.]])
-    O = np.array([[0.,0.,0.],[0.,0.,0.],[0.,0.,0.]])
-    for i in range(3):
-        for j in range(3):
-            S[i][j] = 0.5*(J[j][i]+J[i][j])
-            O[i][j] = 0.5*(J[j][i]-J[i][j])
-
-    return S,O
