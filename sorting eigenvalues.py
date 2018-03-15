4d2cc102c7caab8172faeed6e6e436fec1e8b3cb
leannevandam <36501611+leannevandam@users.noreply.github.com>
2018-03-15 10:36:16 +0100

    Delete sorting eigenvalues.py

--- sorting eigenvalues.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numpy import linalg as LA
-
-#insert a matrix at 'HERE' as [[a,b,c],[d,e,f],[g,h,i]], so the rows of the matrix as seperate lists
-
-evalues,y = LA.eig(np.array('HERE'))
-evalues = np.sort(evalues)
-
-print evalues
