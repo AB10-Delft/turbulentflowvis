ce6da0aa78b1cf3af2015d06a628070f910fcb43
MikeLitoris <37401741+MikeLitoris@users.noreply.github.com>
2018-03-16 15:13:33 +0200

    Update jacobian_file.py

--- jacobian_file.py
+++ jacobian_file.py
@@ -1,6 +1,5 @@
 import scipy.io as sc
 import numpy as np
-import numdifftools as nd
 import timeit
 
 ### This version uses predefined tools:
@@ -69,6 +68,8 @@ def backward(mtx,i,j,k,comp, drct, step = 1.):
         prim = (mtx[i,j,k][vel]-mtx[i,j,k-1][vel])/step
         return prim
 
+    
+    
 def jacobian_point(mtx,i,j,k):
     """ Returns the 3x3 for a given point"""
     velocities = ['u','v','w']
@@ -84,6 +85,8 @@ def jacobian_point(mtx,i,j,k):
             else:
                 temp[n,m] = central(mtx,i,j,k,comp,drct)
     return temp
+
+
 
 
 def jacobian(mtx):
@@ -108,38 +111,6 @@ def jacobian(mtx):
                 J[i,j,k] = temp
                 #print temp
     return J
-
-
-######  read the matlabfile
-##dt = sc.loadmat('raw_data_1.mat')
-##
-##### using correct keys, obtain three 3D matrixes, representing u,v,w velocities (scalar fields)
-##vel_u =  dt['u']
-##vel_w =  dt['w']
-##vel_v = dt['v']
-##
-##
-##### transform the matrixes into arrays, because arrays are "better"
-##vel_u = np.squeeze(np.asarray(vel_u))
-##vel_w = np.squeeze(np.asarray(vel_w))
-##vel_v = np.squeeze(np.asarray(vel_v))
-##
-##
-##
-##### combine all arrays
-##
-##C = np.zeros((192,192,192),dtype = 'object')
-##
-##for i in range(192):
-##    for j in range(192):
-##        for k in range(192):
-##            C[i,j,k] = [vel_u[i,j,k],vel_v[i,j,k],vel_w[i,j,k]]
-##    
-##               
-##ans = jacobian(C)
-##
-##    
-   
     
     
     
